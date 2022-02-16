6888d99ddd53e43750e70b595986c4a83b852405
Jenny Kibiri <=>
2022-02-16 12:26:30 +0300

    add bootstrap

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,23 @@
-import React from 'react'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React from "react";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 const Home = () => {
   return (
     <div>
-
       <Head>
         <title>nextjs blog</title>
         <link rel="icon" href="/blog.png" />
         <meta property="og:title" content="blog home" key="title" />
       </Head>
 
-<main >
-<div className={styles.container}>
-
-  <h1>Hello Next.js</h1>
-  </div>
-</main>
-
+      <main>
+        <div className={styles.container}>
+          <h1 class="text-primary">Hello Next.js</h1>
+        </div>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
