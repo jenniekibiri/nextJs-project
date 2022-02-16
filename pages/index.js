339c3ae485b6be552b43801efb71bed6f341d054
Jenny Kibiri <=>
2022-02-16 15:01:24 +0300

    create json data

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,24 @@ const Home = () => {
       <Nav />
       <main>
         <div className={styles.container}>
-          <h1 class="text-primary">Hello Next.js</h1>
+          <div className="row">
+            <div className="col-md-4">
+              <div class="card" style={{width: "18rem"}}>
+                <img src="..." class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <a href="#" class="btn btn-primary">
+                    Go somewhere
+                  </a>
+                </div>
+              </div>
+              
+            </div>
+                      </div>
         </div>
       </main>
     </div>
